26500e42992fbdf0a76d2119bff3790ca4ecd82d
Antonio do Nascimento de Souza <antonionascimentosouza23@gmail.com>
2025-06-07 22:39:03 -0300

    Adiciona estrutura inicial com componentes React

---
+++ Mercado-Bom-Preco/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Mercado Bom Preço</h1>
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#produtos">Produtos</a>
+        <a href="#contato">Contato</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Mercado-Bom-Preco/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <section id="home">
+      <h2>Bem-vindo ao Mercado Bom Preço!</h2>
+      <p>Aqui você encontra os melhores produtos com preços incríveis.</p>
+    </section>
+  );
+}
+
+export default Home;
